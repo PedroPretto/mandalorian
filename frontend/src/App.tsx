1f3958616acd61703895bdcc6eaec570ed814d0d
pedro.martins <pedro.martins@khomp.com>
2021-02-01 09:36:14 -0300

    corrigida viewport no mobile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React from 'react'
 import Banner from './components/Banner'
 import Wrapper from './components/Wrapper'
 import LoginContainer from './components/LoginContainer'
 function App() {
+
+  let vh = window.innerHeight * 0.01
+  document.documentElement.style.setProperty('--vh', `${vh}px`)
+
   return (
     <div className="App">
       <Wrapper 
@@ -10,7 +14,7 @@ function App() {
       loginContainer={<LoginContainer/>}
       />
     </div>
-  );
+  )
 }
 
 export default App;
